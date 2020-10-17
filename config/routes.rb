f1125217c18504e235997860b20e2df68ed6ec0c
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-10-16 21:34:22 -0400

    added basic user routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
 
   root "static_pages#root"
+  namespace :api, defaults: {format: :json} do
+
+    resources :users, only: [:create]
+    resource :session, only: [:destroy, :create]
+   
+
+  end
 end
