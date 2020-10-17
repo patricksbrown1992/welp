215ef287856fb305841f85e9d43e53b9ed8ba884
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-10-16 21:34:03 -0400

    changed first name and last name columns

--- db/migrate/20201017010232_add_first_name_and_last_name_to_users.rb
+++ db/migrate/20201017010232_add_first_name_and_last_name_to_users.rb
@@ -1,6 +1,6 @@
 class AddFirstNameAndLastNameToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :firstName, :string, null: false
-    add_column :users, :lastName, :string, null: false
+    add_column :users, "First Name", :string, null: false
+    add_column :users, "Last Name", :string, null: false
   end
 end
