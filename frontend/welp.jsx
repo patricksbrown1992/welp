713cf25b80465aa47aa23972f31a661e988961eb
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-10-19 13:52:27 -0400

    added entry doc

--- frontend/welp.jsx
+++ frontend/welp.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener("DOMContentLoaded", () => {
-  const root = document.getElementById("root");
-  ReactDOM.render(<h1>Welp</h1>, root);
+  let store;
+  if (window.currentUser) {
+      const preloadedState = {
+          entities: {
+              user: { [window.currentUser.id]: window.currentUser }
+          },
+          session: { id: window.currentUser.id }
+      };
+      store = configureStore(preloadedState);
+      delete window.currentUser;
+  } else {
+      store = configureStore();
+  }
+
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={store} />, root)
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 });
